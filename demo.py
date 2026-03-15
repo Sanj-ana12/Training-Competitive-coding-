523a3ed39191893aa018ad51e5fdf51e1eb7a620
Sanjana Rajput <24rajputs@rbunagpur.in>
2026-03-15 22:42:50 +0530

    COMPETATIVE CODING

---
+++ demo.py
@@ -0,0 +1,35 @@
+# program for simple interest 
+#principle = 100000
+#roi = 10
+#time = 1
+#si = principle*roi*time/ 100
+#print("Simple interest = ",si)
+
+# c to f 
+#celcius = 25
+#fahrenheit = (celcius * 9/5) + 32
+#print("temperature in fahrenheit: ",fahrenheit)
+
+# SWAPING 
+#val1 = int(input("enter the first number: "))
+#print("before swapping: ",val1, val2)
+#temp = val1
+#val1 = val2
+#val2 = temp
+#print("after swapping: ",val1,val2)
+
+
+#h = float(input("enter the height: "))
+#inch = h*12
+#cm = inch*2.54
+#print("height in inch: ",inch)
+#print("height in cm: ",cm)
+
+num =1234567
+print(num)
+a = num % 10
+num = num // 10
+b = num % 10
+c = num // 10
+rev = a*100 + b*10 + c*1
+print(rev)
